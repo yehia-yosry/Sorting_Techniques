3022d17f7ef873db0b4153e382c045870432434e
Yehia Yosry <yehiayosry200@gmail.com>
2025-02-28 10:56:11 +0200

    Added graph visualization

--- Sorting_Techniques.py
+++ Sorting_Techniques.py
@@ -1,18 +1,25 @@
 import time
 import random
+import numpy as np
+import matplotlib.pyplot as plt # type: ignore
+
 
 def bubble_sort(arr):
+    start = time.perf_counter()
     for i in range(0, len(arr)):
         is_sorted = True
         for j in range(0, len(arr) - 1 - i):
-            if(arr[j] > arr[j+1]):
+            if (arr[j] > arr[j+1]):
                 is_sorted = False
                 arr[j], arr[j+1] = arr[j+1], arr[j]
         if is_sorted == True:
             break
-    return arr
+    end = time.perf_counter()
+    return arr, end - start
+
 
 def insertion_sort(arr):
+    start = time.perf_counter()
     for i in range(1, len(arr), 1):
         value = arr[i]
         pos = i
@@ -23,45 +30,57 @@ def insertion_sort(arr):
             else:
                 break
         arr[pos] = value
-    return arr
+    end = time.perf_counter()
+    return arr, end - start
+
 
 def selection_sort(arr):
+    start = time.perf_counter()
     for i in range(0, len(arr)):
         min_index = i
         for j in range(i+1, len(arr)):
             if arr[j] < arr[min_index]:
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
+    end = time.perf_counter()
+    return arr, end - start
 
-# Random Array Generating Function
-def rand_arr(length):
+
+def random_array(length):
     arr = []
     for i in range(0, int(length)):
-        arr.insert(0, random.randint(0, 12000))
-    print("Random Array:     ",  arr)
-    return arr 
-        
-  
-arr_length = input("Enter Array Length To Test Sorting Algorithms On: ")
-arr = rand_arr(arr_length)
-
-# Testing Bubble Sort
-begin1 = time.time()
-print("Bubble Sorted:    ", bubble_sort(arr),)
-end1 = time.time()
-        
-# Testing Insertion Sort
-begin2 = time.time()
-print("Insertion Sorted: ", insertion_sort(arr))
-end2 = time.time()
-
-# Testing Selection Sort
-begin3 = time.time()
-print("Selection Sorted: ", selection_sort(arr))
-end3 = time.time()
+        arr.insert(0, random.randint(0, 100))
+    return arr
 
 
-print("Bubble Sort Time (us): " ,  (end1 - begin1) * 1000)
-print("Insertion Sort Time (us): " ,  (end2 - begin2) * 1000)
-print("Selection Sort Time (us): " ,  (end3 - begin3) * 1000)
+def time_complexity(input_sizes):
+    time_bubble = []
+    time_insertion = []
+    time_selection = []
+    for i in range(0, len(input_sizes)):
+        arr = random_array(input_sizes[i])
+        time_bubble.append(bubble_sort(arr)[1])
+        time_insertion.append(insertion_sort(arr)[1])
+        time_selection.append(selection_sort(arr)[1])
+    return time_bubble, time_insertion, time_selection
+
+
+input_sizes = [0, 100, 200, 300, 500]
+time_ = time_complexity(input_sizes)
+bubble_sort_time = time_[0]
+insertion_sort_time = time_[1]
+selection_sort_time = time_[2]
+
+
+plt.plot(input_sizes, bubble_sort_time, marker='o',
+         linestyle='-', color='r', label='Bubble Sort')
+plt.plot(input_sizes, insertion_sort_time, marker='o',
+         linestyle='-', color='g', label='Insertion Sort')
+plt.plot(input_sizes, selection_sort_time, marker='o',
+         linestyle='-', color='b', label='Selection Sort')
+
+plt.xlabel("Input Size(N)")
+plt.ylabel("Execution Time (microsec)")
+plt.title("Time Complexity Graph")
+plt.legend()
+plt.show()
